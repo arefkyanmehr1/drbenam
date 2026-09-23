8122a5c3d7836cd9389cb4e8e67647f890d48321
arefkyanmehr1 <arefkyanmehr9@gmail.com>
2026-09-23 21:47:45 +0330

    Trigger verified Android build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.aistudio.webtoapp.kzpmx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
+    versionCode = 2
     versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
